59693a6b1355788dc6238616b5b9718aafbb3f20
gaurav-sharma95 <gaurav.sharma95.gs@gmail.com>
2024-06-03 16:33:23 +0530

    add base java code

---
+++ automation/src/main/java/demo/MicroserviceApplication.java
@@ -0,0 +1,27 @@
+package demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class MicroserviceApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(MicroserviceApplication.class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+		System.out.println("Microservice!!");
+	}
+	
+	@RequestMapping(value = "/hello")
+	public String helloWorld()
+	{
+		return "Hello World, Peter";
+	}
+}
